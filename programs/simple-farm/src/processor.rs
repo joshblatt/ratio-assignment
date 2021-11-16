0137db1af74e1d1a7a3e60bdc34b7e718c1d5f92
SolanaEngineer <topstack2021a@gmail.com>
2021-11-16 17:55:01 +0300

    upload

---
+++ programs/simple-farm/src/processor.rs
@@ -0,0 +1,4 @@
+
+
+pub mod process_create_global_state;
+pub use process_create_global_state::*;
---
+++ programs/simple-farm/src/processor/process_create_global_state.rs
@@ -0,0 +1,12 @@
+use anchor_lang::prelude::*;
+
+use crate::{
+    states::*,
+    error::*,
+    constant::*,
+    instructions::*
+};
+
+pub fn process_create_global_state(ctx: Context<CreateGlobalState>) -> ProgramResult {
+    Ok(())
+}
