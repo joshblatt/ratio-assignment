7bcab4042c97f0e0e9b76232cd9b9d60be0b9379
Josh Blatt <thejoshblatt@yahoo.com>
2022-01-05 22:21:10 -0500

    Setting up repo structure (compiling and passing test)

--- simple-cpi-2021-11-29/simple-cpi/programs/mint-and-deposit/src/lib.rs
+++ simple-cpi-2021-11-29/simple-cpi/programs/mint-and-deposit/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod simple_cpi {
+pub mod mint_and_deposit {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
         Ok(())
--- simple-cpi-2021-11-29/simple-cpi/tests/simple-cpi.ts
+++ simple-cpi-2021-11-29/simple-cpi/tests/simple-cpi.ts
@@ -1,17 +1,21 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
-import { SimpleCpi } from '../target/types/simple_cpi';
+import { MintAndDeposit } from '../target/types/mint_and_deposit';
+import { Pool } from '../target/types/pool';
 
 describe('simple-cpi', () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
-  const program = anchor.workspace.SimpleCpi as Program<SimpleCpi>;
+  const mint_and_deposit_program = anchor.workspace.MintAndDeposit as Program<MintAndDeposit>;
+  const pool_program = anchor.workspace.Pool as Program<Pool>;
 
   it('Is initialized!', async () => {
     // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+    const mint_tx = await mint_and_deposit_program.rpc.initialize({});
+    const pool_tx = await pool_program.rpc.initialize({});
+    console.log("Your Mint + Deposit transaction signature", mint_tx);
+    console.log("Your Pool transaction signature", mint_tx);
   });
 });
