8b6299b921f404b7502521e1738d542161f61d9b
Josh Blatt <josh@ark-trading.com>
2022-01-09 20:18:38 -0500

    Reworking deposit function

--- simple-cpi-2021-11-29/simple-cpi/programs/pool/src/lib.rs
+++ simple-cpi-2021-11-29/simple-cpi/programs/pool/src/lib.rs
@@ -1,71 +1,78 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
+use anchor_spl::token::{self, Token, TokenAccount, Transfer};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod pool {
     use super::*;
-    pub fn deposit_wrapper(ctx: Context<DepositWrapper>, amount: u64) -> ProgramResult {
-        msg!("Starting Tokens: {}", ctx.accounts.sender_token.amount);
-        token::transfer(ctx.accounts.transfer_ctx(), amount)?;
-        ctx.accounts.sender_token.reload()?;
-        msg!("Remaining Tokens: {}", ctx.accounts.sender_token.amount);
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> ProgramResult {
+        msg!("Starting Tokens: {}", ctx.accounts.depositer_token_account.amount);
+        token::transfer(ctx.accounts.deposit_ctx(), amount)?;
+        ctx.accounts.depositer_token_account.reload()?;
+        msg!("Remaining Tokens: {}", ctx.accounts.depositer_token_account.amount);
         Ok(())
     }
-    pub fn withdraw_wrapper(ctx: Context<WithdrawWrapper>, amount: u64) -> ProgramResult {
-        msg!("Starting Tokens: {}", ctx.accounts.receiver_token.amount);
-        token::transfer(ctx.accounts.transfer_ctx(), amount)?;
-        ctx.accounts.receiver_token.reload()?;
-        msg!("Remaining Tokens: {}", ctx.accounts.receiver_token.amount);
-        Ok(())
-    }
+    // pub fn withdraw_wrapper(ctx: Context<WithdrawWrapper>, amount: u64) -> ProgramResult {
+    //     msg!("Starting Tokens: {}", ctx.accounts.receiver_token.amount);
+    //     token::transfer(ctx.accounts.transfer_ctx(), amount)?;
+    //     ctx.accounts.receiver_token.reload()?;
+    //     msg!("Remaining Tokens: {}", ctx.accounts.receiver_token.amount);
+    //     Ok(())
+    // }
 }
 
+// #[account]
+// pub struct PoolAccount {
+//     pub owner: Pubkey, // user that the pool account belongs to
+//     pub authority: Pubkey, // PDA for pool
+// }
+
 #[derive(Accounts)]
-pub struct DepositWrapper<'info> {
-    pub sender: Signer<'info>,
+pub struct Deposit<'info> {
+    pub depositer: Signer<'info>, // depositers account
     #[account(mut)]
-    pub sender_token: Account<'info, TokenAccount>,
+    pub depositer_token_account: Account<'info, TokenAccount>, // depositers token account
+    // pub pool_account: Account<'info, PoolAccount>, // pool account for depositer
+    pub pool_account: AccountInfo<'info>, // 
     #[account(mut)]
-    pub receiver_token: Account<'info, TokenAccount>,
-    pub mint: Account<'info, Mint>,
+    pub pool_token_account: Account<'info, TokenAccount>, // pool token account for depositer, pool is authority
     pub token_program: Program<'info, Token>,
 }
 
-impl<'info> DepositWrapper<'info> {
-    fn transfer_ctx(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+impl<'info> Deposit<'info> {
+    fn deposit_ctx(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
         CpiContext::new(
             self.token_program.to_account_info(),
             Transfer {
-                from: self.sender_token.to_account_info(),
-                to: self.receiver_token.to_account_info(),
-                authority: self.sender.to_account_info(),
+                from: self.depositer_token_account.to_account_info(),
+                to: self.pool_token_account.to_account_info(),
+                authority: self.depositer.to_account_info(),
             },
         )
     }
 }
 
-#[derive(Accounts)]
-pub struct WithdrawWrapper<'info> {
-    pub sender: Signer<'info>,
-    #[account(mut)]
-    pub sender_token: Account<'info, TokenAccount>,
-    #[account(mut)]
-    pub receiver_token: Account<'info, TokenAccount>,
-    pub mint: Account<'info, Mint>,
-    pub token_program: Program<'info, Token>,
-}
+// #[derive(Accounts)]
+// pub struct WithdrawWrapper<'info> {
+//     pub sender: Signer<'info>,
+//     #[account(mut)]
+//     pub sender_token: Account<'info, TokenAccount>,
+//     #[account(mut)]
+//     pub receiver_token: Account<'info, TokenAccount>,
+//     pub mint: Account<'info, Mint>,
+//     pub token_program: Program<'info, Token>,
+// }
 
-impl<'info> WithdrawWrapper<'info> {
-    fn transfer_ctx(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-        CpiContext::new(
-            self.token_program.to_account_info(),
-            Transfer {
-                from: self.sender_token.to_account_info(),
-                to: self.receiver_token.to_account_info(),
-                authority: self.sender.to_account_info(),
-            },
-        )
-    }
-}
+// impl<'info> WithdrawWrapper<'info> {
+//     fn transfer_ctx(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+//         CpiContext::new(
+//             self.token_program.to_account_info(),
+//             Transfer {
+//                 from: self.sender_token.to_account_info(),
+//                 to: self.receiver_token.to_account_info(),
+//                 authority: self.sender.to_account_info(),
+//             },
+//         )
+//     }
+// }
